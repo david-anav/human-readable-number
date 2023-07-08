7e2593294e141f740e748b1e96e02933aa255212
david-anav <art_migalev@yahoo.com>
2023-07-08 09:12:50 +0300

    fix: '<12'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 // module.exports = function toReadable (number) {
 
 // }
-let units = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', ''];
-let dozens = [ 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+let units = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'Twelve'];
+let dozens = 'ty';
+let less20 = 'teen';
 let hundred = 'hundred';
 
-let  num= 7 ;
+let  num= 1 ;
 let count = num.toString()
-console.log(typeof(count))
-if ( count.length=== 1 ) {
+// console.log(typeof(count))
+if ( count.length <= 2 && num <= 12) {
     console.log(units[num-1]);
 }
 // if (num.length === 2) {
