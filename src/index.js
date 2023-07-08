fb61a59fd02d5afff4c9c344b1438d15cd6ae0df
david-anav <art_migalev@yahoo.com>
2023-07-08 08:58:44 +0300

    fix: 'readable units'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
-  
+// module.exports = function toReadable (number) {
+
+// }
+let units = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', ''];
+let dozens = [ 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+let hundred = 'hundred';
+
+let  num= 7 ;
+let count = num.toString()
+console.log(typeof(count))
+if ( count.length=== 1 ) {
+    console.log(units[num-1]);
 }
+// if (num.length === 2) {
+
+// }
+// if  ( num.length === 3) {
+
+// }
