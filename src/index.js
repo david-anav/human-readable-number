391d6ef97ece1cf7036b9581720eb4db8f19381c
david-anav <art_migalev@yahoo.com>
2023-07-08 09:18:51 +0300

    fix: 'add dozens'

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ let dozens = 'ty';
 let less20 = 'teen';
 let hundred = 'hundred';
 
-let  num= 1 ;
+let  num= 13 ;
 let count = num.toString()
 // console.log(typeof(count))
 if ( count.length <= 2 && num <= 12) {
     console.log(units[num-1]);
 }
-// if (num.length === 2) {
-
-// }
+if ( num > 12 && num < 20) {
+    console.log(`${units[num%10]}${dozens}`)
+}
 // if  ( num.length === 3) {
 
 // }
