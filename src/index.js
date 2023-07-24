72fbee7dafbaa80c75ac981142cf9c41935db0b1
david-anav <art_migalev@yahoo.com>
2023-07-24 22:33:20 +0300

    feat: "finish algorithmic function'

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-// module.exports = function toReadable (number) {
-
+// function dz(number){
+//     return Math.trunc((number%100)
 // }
-let units = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'Twelve'];
-let dozens = 'ty';
-let less20 = 'teen';
-let hundred = 'hundred';
+const units = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+const shablTy = 'ty'; /*60*/
+const dozens = [ 'twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine'];
+const less20 = 'teen';   /*19*/
+const hundred = 'hundred';
+module.exports = function toReadable (number) {
 
-let  num= 13 ;
-let count = num.toString()
-// console.log(typeof(count))
-if ( count.length <= 2 && num <= 12) {
-    console.log(units[num-1]);
-}
-if ( num > 12 && num < 20) {
-    console.log(`${units[num%10]}${dozens}`)
-}
-// if  ( num.length === 3) {
 
-// }
+    let count = number.toString()
+    if (number === 0 ){
+        return ('zero');
+    }
+    if (count.length <= 2) {
+        if (number <= 12) {
+            return (units[number-1]);
+        }else if (number > 12 && number < 20 && number !==14){
+            return (`${dozens[number%10-2]}${less20}`);
+        }else if ( number === 14){
+            return (`${units[3]}${less20}`)
+        }else if(number>=20 && number<=99 ){
+            if(number%10 === 0){
+                return (`${dozens[number/10-2]}${shablTy}`)
+            }else if(number%10!==0){
+                return (`${dozens[Math.trunc(number/10)-2]}${shablTy} ${units[number%10-1]}`)
+            }
+        }
+    }else if (count.length === 3){
+        if (number%100 === 0){
+            return (`${units[(number/100)-1]} ${hundred}`)
+        }else if(number%100 !==0){
+            if (number%100 <= 12) {
+                return (`${units[Math.trunc(number/100)-1]} ${hundred} ${units[number%100-1]}`);
+            }else if ( number%100 === 14 ){
+                return (`${units[Math.trunc(number/100)-1]} ${hundred} ${units[3]}${less20}`)
+            }else if (number%100 > 12 && number%100 <20 ){
+                return (`${units[Math.trunc(number/100)-1]} ${hundred} ${dozens[(number%100)%10-2]}${less20}`)
+            }else if ( number%100 >=20 && number%100%10 ===0){
+                return (`${units[Math.trunc(number/100)-1]} ${hundred} ${dozens[(number%100)/10-2]}${shablTy}`)
+            }else if ( number%100 >=20 && number%100%10 !==0) {
+                return (`${units[Math.trunc(number/100)-1]} ${hundred} ${dozens[Math.trunc((number%100)/10)-2]}${shablTy} ${units[number%100%10-1]}`)
+            }
+        }
+    }
+}
+// console.log(toReadable(914))
+// let  number= 941;
